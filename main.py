f7fdd7d1c20ee1455a04a3a75c1a2ee5245fefad
Lasantha Mudalige <lasantha1130@gmail.com>
2022-08-18 22:52:19 +0530

    pass current year for every page

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 from flask import Flask, render_template
+from datetime import date
 
 app = Flask(__name__)
+
+now = date.today().year
 
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", year=now)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", year=now)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", year=now)
 
 
 @app.route("/resume")
 def resume():
-    return render_template("resume.html")
+    return render_template("resume.html", year=now)
 
 
 if __name__ == "__main__":
